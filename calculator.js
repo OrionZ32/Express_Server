a67ff2672edcf2b185851a436e2e7a178c9314ea
Harley Garcia <hagarcia99@outlook.com>
2020-08-11 21:59:35 -0500

    BMi calculator

--- calculator.js
+++ calculator.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
@@ -19,6 +20,20 @@ app.post("/", function(req, res) {
     res.send("The result of the both numbers added is " + result);
 });
 
-app.listen(3000, function() {
-    console.log("Server has started on port 3000");
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html")
 });
+
+app.post("/bmicalculator", function(req, res) {
+   
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI is " + bmi);
+});
+
+app.listen(3000, function() {
+   console.log("Server has started on port 3000");
+});                                           
